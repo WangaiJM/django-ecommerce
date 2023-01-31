44c2d1876f66d2e6f3eff278705519475652d334
WangaiJM <jahkeyjohn@gmail.com>
2023-01-31 12:36:05 +0300

    add products functionality

--- store/admin.py
+++ store/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import *
 
-# Register your models here.
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['name', 'price', 'digital']
+    list_filter = ['digital']
+    search_fields = ['name']
+
+
+admin.site.register(Customer)
+admin.site.register(Product, ProductAdmin)
+admin.site.register(Order)
+admin.site.register(OrderItem)
+admin.site.register(ShippingAddress)
